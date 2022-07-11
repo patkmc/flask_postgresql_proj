892fb2ab3f2c60555a722179f2f3aa0e587ceddd
Patyrk Kmieciński <patryk.kmiecinski@deviniti.com>
2022-07-11 15:04:37 +0200

    Add logging

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,3 +1,7 @@
+import logging
+from logging import StreamHandler
+from logging.handlers import TimedRotatingFileHandler
+
 from flask import Flask
 from flask_app.author import blueprints as book_bp
 from flask_app.book import blueprints as author_bp
@@ -6,9 +10,27 @@ from flask_app.extensions import db
 from flask_app.extensions import migrate
 
 
+def _configure_logging(flask_cfg):
+    formatter = logging.Formatter("%(asctime)s: %(threadName)s : %(levelname)s: %(message)s")
+
+    handlers = []
+    # create logger with file handler
+    file_logger = TimedRotatingFileHandler(f"{flask_cfg.LOG_FILE_PATH}/{flask_cfg.LOG_FILE_NAME}", when="midnight")
+    file_logger.setLevel(flask_cfg.LOG_LEVEL)
+    file_logger.setFormatter(formatter)
+    handlers.append(file_logger)
+
+    if flask_cfg.MODE == "development":
+        handlers.append(StreamHandler())
+
+    logging.basicConfig(level=flask_cfg.LOG_LEVEL, handlers=handlers)
+
+
 def create_app(test_config=False):
     app = Flask(__name__)
-    app.config.from_object(config_factory(test_config))
+    flask_cfg = config_factory(test_config)
+    app.config.from_object(flask_cfg)
+    _configure_logging(flask_cfg)
     db.init_app(app)
     migrate.init_app(app, db=db)
 
--- flask_app/config.py
+++ flask_app/config.py
@@ -2,11 +2,16 @@ import os
 
 
 class Config:
+    MODE = os.getenv("MODE")
     DB_USERNAME = os.getenv("DB_USERNAME")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_NAME = os.getenv("DB_NAME")
     DB_HOST = os.getenv("DB_HOST")
     DB_PORT = os.getenv("DB_PORT")
+
+    LOG_FILE_PATH = os.getenv("LOG_FILE_PATH")
+    LOG_FILE_NAME = os.getenv("LOG_FILE_NAME")
+    LOG_LEVEL = os.getenv("LOG_LEVEL")
 
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
@@ -16,7 +21,7 @@ class TestConfig(Config):
     TESTING = True
 
 
-def config_factory(test_config: bool):
+def config_factory(test_config: bool) -> Config | TestConfig:
     if test_config:
         return TestConfig()
     return Config()
